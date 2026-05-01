eb10837f8c02b9c92bb50e55022231fcc5096f50
Rajesh Parimi <77872544+rajeshparimi22@users.noreply.github.com>
2026-05-01 17:11:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
 
-import Home from "./components/Hero";
 import About from "./components/About";
 import Products from "./components/Products";
 import Contact from "./components/Contact";
-import SignupLogin from "./components/SignupLogin"; 
+import SignupLogin from "./components/SignupLogin";
 
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
-
-        {/* Contact page */}
         <Route path="/contact" element={<Contact />} />
-
-        {/* ✅ Signup/Login page */}
         <Route path="/auth" element={<SignupLogin />} />
       </Routes>
 
       <Footer />
-    </div>
-    
+    </BrowserRouter>
   );
 }
 
 export default App;
-export default function App() {
-  return <h1>Working</h1>;
-}
